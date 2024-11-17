844e5aabe40b5da97f07d504bdb461810b32bdd7
Bradley Wood <bwood1@butler.edu>
2024-11-17 00:13:00 -0500

    fix

--- src/portfolio.js
+++ src/portfolio.js
@@ -332,7 +332,7 @@ const blogSection = {
   subtitle:
     "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
   displayMediumBlogs: true,
-  blogs:{},
+  blogs: [],
   display: true // Set false to hide this section, defaults to true
 };
 
